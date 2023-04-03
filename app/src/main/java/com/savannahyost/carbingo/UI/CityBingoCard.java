2474114fd4ffc3408065e68b0271c950e7316ddf
Savannah Yost <savannahyost85@gmail.com>
2023-04-02 21:28:21 -0400

    Added city bingo card grid

--- app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
+++ app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
@@ -12,5 +12,7 @@ public class CityBingoCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_bingo_card);
+
+
     }
 }
