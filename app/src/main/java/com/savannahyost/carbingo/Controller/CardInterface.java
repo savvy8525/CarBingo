067a510f254a74e1e7c6251a1422c268a009f743
Savannah Yost <savannahyost85@gmail.com>
2023-04-13 21:03:37 -0400

    fixed transparency to add grid back in on clicks - Mac

---
+++ app/src/main/java/com/savannahyost/carbingo/Controller/CardInterface.java
@@ -0,0 +1,8 @@
+package com.savannahyost.carbingo.Controller;
+
+import android.widget.Button;
+
+public interface CardInterface {
+
+    Button[] setIdButton();
+}
--- app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
+++ app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
@@ -7,16 +7,19 @@ import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.savannahyost.carbingo.Controller.CardController;
+import com.savannahyost.carbingo.Controller.CardInterface;
 import com.savannahyost.carbingo.R;
 
 
 public class CityBingoCard extends AppCompatActivity {
+//    private CardInterface cardInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +39,7 @@ public class CityBingoCard extends AppCompatActivity {
         Button[] buttons = new Button[buttonId.length];
         int buttonNum = 0;
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = (Button)findViewById(buttonId[i]);
+            buttons[i] = findViewById(buttonId[i]);
 
         }
 
@@ -44,93 +47,30 @@ public class CityBingoCard extends AppCompatActivity {
             for (int j = 0; j < randomItems.length; j++) {
                 buttons[buttonNum].setText(randomItems[i][j]);
                 buttonNum++;
-//                System.out.println(buttonNum);
             }
         }
-
-        boolean[][] isClicked = new boolean[5][5];
-        for (int i = 0; i < isClicked.length; i++) {
-            for (int j = 0; j < isClicked.length; j++) {
-                isClicked[i][j] = false;
-            }
-        }
-
 
         boolean[] clicked = new boolean[25];
 
         for (int i = 0; i < buttons.length; i++) {
             buttons[i].setOnClickListener(new View.OnClickListener() {
-                
+                int backgroundOff = R.drawable.button_background_off;
+//                Drawable buttonOff = getResources().getDrawable()
                 @Override
                 public void onClick(View v) {
-                    int buttonPress = getColor(R.color.button_press_1);
-
-
                     for (int j = 0; j < buttons.length; j++) {
-
                          if(buttons[j].isPressed() && clicked[j]) {
-                            buttons[j].setBackgroundColor(Color.TRANSPARENT);
+                            buttons[j].setBackgroundResource(R.drawable.button_background_off);
                             clicked[j] = false;
+                             System.out.println(clicked[j]);
                         } else if(buttons[j].isPressed()) {
                             buttons[j].setBackgroundColor(R.drawable.button_background);
                             clicked[j] = true;
                         }
-
                     }
-
-
                 }
             });
         }
-
-
-
-
-//button1_1.is
-//        button1_1.setBackgroundColor(Color.GREEN);
-//        button1_1.setOnClickListener(new View.OnClickListener() {
-////            int click = 1;
-//boolean notClicked = true;
-//            int count = 0;
-//            @Override
-//            public void onClick(View v) {
-//                if(notClicked) {
-//                    button1_1.setBackgroundColor(R.drawable.button_background);
-////                    click = 2;
-//                    notClicked = false;
-//                    isClicked[0][0] = true;
-////                    System.out.println(click);
-//                } else {
-//                    button1_1.setBackgroundColor(Color.TRANSPARENT);
-////                    click = 1;
-//                    notClicked = true;
-//                    isClicked[0][0] = false;
-//                    count = 0;
-////                    System.out.println(click);
-//                }
-//
-//                for (int i = 0; i < isClicked.length; i++) {
-//                    if(isClicked[0][i]) {
-////                        hasBingo = true;
-//                        count++;
-//                    } else {
-//                        count = 0;
-//                    }
-//                }
-//
-//                if(count == 5) {
-//                    Toast.makeText(CityBingoCard.this, "BINGO!", Toast.LENGTH_LONG).show();
-//                    System.out.println("bingo!");
-//                }
-//
-//            }
-//        });
-
-
-    }
-
-    public void deselectButton() {
-
     }
 
 }
