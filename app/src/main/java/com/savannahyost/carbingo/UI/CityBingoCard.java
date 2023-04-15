649ef6e26a0a0920391cc83c3a705d855f0903cb
Savannah Yost <savannahyost85@gmail.com>
2023-04-15 17:34:04 -0400

    setup rows to bingo - Mac

--- app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
+++ app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
@@ -10,13 +10,15 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.savannahyost.carbingo.Controller.CardController;
 import com.savannahyost.carbingo.R;
 
+import java.util.Arrays;
+
 
 public class CityBingoCard extends AppCompatActivity {
-//    private CardInterface cardInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,48 +28,93 @@ public class CityBingoCard extends AppCompatActivity {
 
         CardController cardController = new CardController();
         String[][] randomItems = cardController.randomCityCard();
-
+        boolean[][] isPressed = new boolean[5][5];
         int[] buttonId = {R.id.button1_1, R.id.button1_2, R.id.button1_3, R.id.button1_4, R.id.button1_5,
                 R.id.button2_1, R.id.button2_2, R.id.button2_3, R.id.button2_4, R.id.button2_5,
                 R.id.button3_1, R.id.button3_2, R.id.button3_3, R.id.button3_4, R.id.button3_5,
                 R.id.button4_1, R.id.button4_2, R.id.button4_3, R.id.button4_4, R.id.button4_5,
                 R.id.button5_1, R.id.button5_2, R.id.button5_3, R.id.button5_4, R.id.button5_5};
-
-        Button[] buttons = new Button[buttonId.length];
         int buttonNum = 0;
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = findViewById(buttonId[i]);
 
-        }
+//        int canBingo = 0;
 
-        for (int i = 0; i < randomItems.length; i++) {
-            for (int j = 0; j < randomItems.length; j++) {
-                buttons[buttonNum].setText(randomItems[i][j]);
+        Button[][] buttonRows = new Button[5][5];
+        for (int i = 0; i < buttonRows.length; i++) {
+            for (int j = 0; j < buttonRows.length; j++) {
+                buttonRows[i][j] = findViewById(buttonId[buttonNum]);
+                buttonRows[i][j].setText(randomItems[i][j]);
                 buttonNum++;
             }
         }
 
-        boolean[] clicked = new boolean[25];
 
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i].setOnClickListener(new View.OnClickListener() {
-                int backgroundOff = R.drawable.button_background_off;
-//                Drawable buttonOff = getResources().getDrawable()
-                @Override
-                public void onClick(View v) {
-                    for (int j = 0; j < buttons.length; j++) {
-                         if(buttons[j].isPressed() && clicked[j]) {
-                            buttons[j].setBackgroundResource(R.drawable.button_not_pressed);
-                            clicked[j] = false;
-                             System.out.println(clicked[j]);
-                        } else if(buttons[j].isPressed()) {
-                            buttons[j].setBackgroundResource(R.drawable.button_pressed);
-                            clicked[j] = true;
+
+        for (int i = 0; i < buttonRows.length; i++) {
+            for (int j = 0; j < buttonRows.length; j++) {
+                buttonRows[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        for (int k = 0; k < buttonRows.length; k++) {
+                            int canBingoRow = 0;
+                            int canBingoColumn = 0;
+                            for (int l = 0; l < buttonRows.length; l++) {
+                                if(buttonRows[k][l].isPressed() && isPressed[k][l]) {
+                                    buttonRows[k][l].setBackgroundResource(R.drawable.button_not_pressed);
+                                    isPressed[k][l] = false;
+                                } else if (buttonRows[k][l].isPressed()) {
+                                    buttonRows[k][l].setBackgroundResource(R.drawable.button_pressed);
+                                    isPressed[k][l] = true;
+                                }
+                                if(isPressed[k][l]) {
+                                    canBingoRow++;
+                                    System.out.println("row: " + canBingoRow);
+                                }else {
+                                    canBingoRow = 0;
+                                }
+//                                if (isPressed[l][k]) {
+//                                    canBingoColumn++;
+//                                    System.out.println("column: " + canBingoColumn);
+//                                } else {
+//                                    canBingoColumn = 0;
+//                                }
+                                if(canBingoRow == 5 || canBingoColumn == 5) {
+                                    System.out.println("bingo");
+                                }
+                            }
+
                         }
+
                     }
-                }
-            });
+                });
+
+//                for (int k = 0; k < isPressed.length; k++) {
+//                    for (int l = 0; l < isPressed.length; l++) {
+//                        System.out.println(isPressed[k][l]);
+//
+//                    }
+//
+//                }
+            }
         }
+
+
+
+//        for (int i = 0; i < isPressed.length; i++) {
+//            for (int j = 0; j < isPressed.length; j++) {
+//                System.out.println(isPressed[i][j]);
+////                if(isPressed[i][j]) {
+////                    canBingo++;
+////                    System.out.println(canBingo);
+////                } else {
+////                    canBingo = 0;
+////                }
+//            }
+//
+//        }
+
+
+
     }
 
 
