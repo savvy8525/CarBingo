556cd258c90e516091f241d5181c5bb19b98732d
Savannah <savannahyost85@gmail.com>
2023-04-15 22:33:34 -0400

    refactored with private methods - PC

--- app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
+++ app/src/main/java/com/savannahyost/carbingo/UI/CityBingoCard.java
@@ -14,22 +14,24 @@ import android.widget.Toast;
 
 import com.savannahyost.carbingo.Controller.CardController;
 import com.savannahyost.carbingo.R;
+import com.savannahyost.carbingo.buttonFactory;
 
 import java.util.Arrays;
 
 
 public class CityBingoCard extends AppCompatActivity {
     private int testColumn = 0;
+//    private boolean[][] bingoCardMatrix = new boolean[5][5];
+    private Button[][] buttonRows = new Button[5][5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_bingo_card);
 
-
         CardController cardController = new CardController();
         String[][] randomItems = cardController.randomCityCard();
-        boolean[][] isPressed = new boolean[5][5];
+
         int[] buttonId = {R.id.button1_1, R.id.button1_2, R.id.button1_3, R.id.button1_4, R.id.button1_5,
                 R.id.button2_1, R.id.button2_2, R.id.button2_3, R.id.button2_4, R.id.button2_5,
                 R.id.button3_1, R.id.button3_2, R.id.button3_3, R.id.button3_4, R.id.button3_5,
@@ -37,7 +39,7 @@ public class CityBingoCard extends AppCompatActivity {
                 R.id.button5_1, R.id.button5_2, R.id.button5_3, R.id.button5_4, R.id.button5_5};
         int buttonNum = 0;
 
-        Button[][] buttonRows = new Button[5][5];
+
         for (int i = 0; i < buttonRows.length; i++) {
             for (int j = 0; j < buttonRows.length; j++) {
                 buttonRows[i][j] = findViewById(buttonId[buttonNum]);
@@ -46,67 +48,64 @@ public class CityBingoCard extends AppCompatActivity {
             }
         }
 
-
+        //TODO: make a onClick private method
         for (int i = 0; i < buttonRows.length; i++) {
             for (int j = 0; j < buttonRows.length; j++) {
 
                 buttonRows[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //TODO: fix the column bingo, it keeps doubling the zero on the first two clicks
-                        //TODO: add in the diagonal bingo
-                        for (int k = 0; k < buttonRows.length; k++) {
-                            int canBingoColumn = 0;
-                            int canBingoRow = 0;
-
-                            for (int l = 0; l < buttonRows.length; l++) {
-                                //Changes button color state and sets a boolean matrix to make "bingo" pop up when there are 5 in a row
-                                if(buttonRows[k][l].isPressed() && isPressed[k][l]) {
-                                    buttonRows[k][l].setBackgroundResource(R.drawable.button_not_pressed);
-                                    isPressed[k][l] = false;
-                                } else if (buttonRows[k][l].isPressed()) {
-                                    buttonRows[k][l].setBackgroundResource(R.drawable.button_pressed);
-                                    isPressed[k][l] = true;
-                                }
-                                //Column bingo - doesn't work well and needs fixed
-                                if(isPressed[l][0]) {
-
-                                    canBingoColumn++;
-
-
-                                }else {
-                                    canBingoColumn = 0;
-                                }
-//                                canBingo++;
-
-                                if(canBingoColumn == 5) {
-                                    System.out.println("bingo");
-                                    canBingoColumn = 0;
-                                }
-                                //Row Bingo - works
-                                if(isPressed[k][l]) {
-//                                    System.out.println("l: " + l + " k: " + k);
-                                    canBingoRow++;
-//                                    System.out.println("row: " + canBingoRow);
-                                }else {
-                                    canBingoRow = 0;
-                                }
-
-                                if(canBingoRow == 5) {
-                                    System.out.println("bingo");
-                                }
-                            }
-                        }
-
+                        onClickView(v);
                     }
-
                 });
+
 
             }
         }
 
 
     }
+
+    //TODO: make this work
+    private void onClickView(View v) {
+
+            for (int k = 0; k < buttonRows.length; k++) {
+                for (int l = 0; l < buttonRows.length; l++) {
+                    //Changes button color state and sets a boolean matrix to make "bingo" pop up when there are 5 in a row
+                    buttonFactory.handleColorChange(buttonRows[k][l]);
+//                    buttonFactory.updateGameMatrix(buttonRows[k][l], k, l);
+//                    buttonFactory.checkRowWin(k,l);
+//                    buttonFactory.checkColumnWin(l,k);
+//                    buttonFactory.checkDiagonalWin(k,l);
+                }
+            }
+
+        }
+
+
+//
+//    private void updateGameMatrix(Button button, int k, int l) {
+//        if(button.isPressed()) {
+//            if(bingoCardMatrix[k][l]) {
+//                bingoCardMatrix[k][l] = false;
+//            } else {
+//                bingoCardMatrix[k][l] = true;
+//            }
+//
+//        }
+//    }
+//
+//    private void checkRowWin() {
+//
+//    }
+//    private void checkColumnWin() {
+//
+//    }
+//
+//    private void checkDiagonalWin() {
+//
+//    }
+
 
 
 }
--- app/src/main/java/com/savannahyost/carbingo/buttonFactory.java
+++ app/src/main/java/com/savannahyost/carbingo/buttonFactory.java
@@ -3,14 +3,72 @@ package com.savannahyost.carbingo;
 import android.widget.Button;
 
 public class buttonFactory {
+    private static boolean[][] bingoCardMatrix = new boolean[5][5];
+//    private static Button[][] buttonRows = new Button[5][5];
 
-    Button[] buttons = new Button[25];
+//    public static void makeBingoCard(int id, int i, int j) {
+//        buttonRows[i][j] = findViewById(id);
+//
+//    }
 
+    //TODO: research ways to keep button from releasing state
+    public static void handleColorChange(Button button) {
+        if(button.isPressed()) {
+            button.setBackgroundResource(R.drawable.button_pressed);
+        }else {
+            button.setBackgroundResource(R.drawable.button_not_pressed);
+        }
+    }
 
+    public static void updateGameMatrix(Button button, int k, int l) {
+        if(button.isPressed()) {
+            if(bingoCardMatrix[k][l]) {
+                bingoCardMatrix[k][l] = false;
+            } else {
+                bingoCardMatrix[k][l] = true;
+            }
 
+        }
+    }
+    //TODO: fix this and confirm functionality
+    public static void checkRowWin(int k, int l) {
+        int canBingoRow = 0;
+        //Row Bingo - works
+        if(bingoCardMatrix[k][l]) {
+//                                    System.out.println("l: " + l + " k: " + k);
+            canBingoRow++;
+//                                    System.out.println("row: " + canBingoRow);
+        }else {
+            canBingoRow = 0;
+        }
 
+        if(canBingoRow == 5) {
+            System.out.println("bingo");
+        }
 
-    public Button[] getButtons() {
-        return buttons;
+    }
+    //TODO: fix the column bingo, it keeps doubling the zero on the first two clicks
+
+    public static void checkColumnWin(int l, int k) {
+        //add for loop
+        int canBingoColumn = 0;
+        //Column bingo - doesn't work well and needs fixed
+        if(bingoCardMatrix[l][k]) {
+
+            System.out.println("l: " + l + " k: " + k);
+            canBingoColumn++;
+
+        }else {
+            canBingoColumn = 0;
+        }
+        if(canBingoColumn == 5) {
+            System.out.println("bingo");
+            canBingoColumn = 0;
+        }
+
+    }
+    //TODO: add in the diagonal bingo
+    public static void checkDiagonalWin(int k, int l) {
+
     }
 }
