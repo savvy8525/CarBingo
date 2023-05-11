1770beec5165105f9cdaa790a9e13a503fbb2172
Savannah <savannahyost85@gmail.com>
2023-05-10 21:53:43 -0400

    continued on animation - PC

--- app/src/main/java/com/savannahyost/carbingo/UI/UIAnimations.java
+++ app/src/main/java/com/savannahyost/carbingo/UI/UIAnimations.java
@@ -1,6 +1,7 @@
 package com.savannahyost.carbingo.UI;
 
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.TextView;
 
 import java.util.logging.ConsoleHandler;
@@ -17,24 +18,42 @@ public class UIAnimations {
 
     //TODO: fix to make the gradient
     public void gradientFadeIn(TextView[] charText) {
+        AlphaAnimation fadeIn1 = new AlphaAnimation(0.0f, 1.0f);
+        AlphaAnimation fadeIn2 = new AlphaAnimation(0.0f, 1.0f);
         AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
 
+
+
+
         int i = 0;
+
+//        charText[0].startAnimation(fadeIn1);
+//        fadeIn1.setDuration(3000);
+//
+//        charText[1].startAnimation(fadeIn2);
+//        fadeIn2.setStartOffset(2000);
+//        fadeIn2.setDuration(3000);
+//
+//        charText[3].startAnimation(fadeIn2);
+//        fadeIn2.setStartOffset(2000);
+//        fadeIn2.setDuration(3000);
+
 //        charText[i].startAnimation(fadeIn);
 //        fadeIn.setDuration(3000);
 //        fadeIn.setFillAfter(true);
 //        fadeIn.setStartOffset(2000);
 //        fadeIn.setFillAfter(true);
 
-
+        int time = 3000;
         while(i < charText.length) {
-        charText[i].startAnimation(fadeIn);
-//        fadeIn.setStartOffset(8000);
+//            charText[i].startAnimation(fadeIn);
+//    //        fadeIn.setStartOffset(8000);
+//
+//            fadeIn.setDuration(time);
+//    //        fadeIn.setFillAfter(true);
+//
+//            time+=1000;
 
-        fadeIn.setDuration(3000);
-        fadeIn.setFillAfter(true);
-
-        
 
 
 //            charText[i].startAnimation(fadeIn);
